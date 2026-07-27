ba8a76bf770cbcddd964e3eb76d7cb8dcb3e28a5
Diogo.W <diogolobao04@gmail.com>
2026-07-27 20:31:08 -0300

    Procurar-personagem-RickMorty
    Agora você consegue escolher algumas opções para aparecer sobre o personagem

--- Api_Rick_and_Morty/script.js
+++ Api_Rick_and_Morty/script.js
@@ -1,24 +1,41 @@
-const characterId =  document.getElementById("characterId")
+const characterId = document.getElementById("characterId")
 const btnGo = document.getElementById("btn-go")
 const content = document.getElementById("content")
 const image = document.getElementById("img")
-// value max 826
+
 // value = ao id 
-const fetchapi = (value) =>{
+const fetchapi = (value) => {
+    if (value > 826) {
+        window.alert("[ERRO!] Os números dos personagens vão até 826")
+    }
     const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        return data
-    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            return data
+        })
 
     return result
+}
+
+//Serve para pegar o que tem na api
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode']
+
+//Serve para criar os getElementById automaticamente
+const buildResult = (result) => {
+    const newObj = {}
+    keys.map((key) => document.getElementById(key)) //Serve para pegar o que está em keys e criar os ID's
+        .map((ele) => { //Serve para verificar se o obj foi selecionado
+            ele.checked && (newObj[ele.name] = result[ele.name])
+        })
+    return newObj
 }
 
 btnGo.addEventListener('click', async (event) => {
     event.preventDefault()
     const result = await fetchapi(characterId.value)
-    content.textContent = `${JSON.stringify(result, undefined, 2)}`
+    //content.textContent = `${JSON.stringify(result, undefined, 2)}`
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
 
     image.src = `${result.image}`
 })
